edcf53358be98ff892dafe4d0e3caa78b1021cf2
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-07-27 21:11:48 +0200

    initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,60 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var _ = require('lodash');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the fabulous ' + chalk.red('ReactZero') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'project',
+      message: "What's your project's name?",
+      default: _.capitalize(_.camelCase(this.appname))
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'), {
+          project: this.props.project
+        }
+      );
+      this.directory(
+        this.templatePath('app'),
+        this.destinationPath('app')
+      );
+      this.directory(
+        this.templatePath('public'),
+        this.destinationPath('public')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+    }
+  },
+
+  install: function () {
+    this.npmInstall();
+  }
+});
---
+++ test/test-app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+var _ = require('lodash');
+
+var project = 'DragonSlayer';
+
+var matchArrayRegExp = function (arr) {
+  var regexStr = _(dependencies).map(function(str) {
+    return '(?=.*' + str + ')';
+  }).join('') + '.+';
+  return new RegExp(regexStr);
+};
+
+describe('react-zero:app', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({ skipInstall: true })
+      .withPrompts({ project: project })
+      .on('end', done);
+  });
+
+  it('creates package.json', function () {
+    assert.file(['package.json']);
+  });
+
+  it('package.json project name is valid', function () {
+    assert.fileContent('package.json', new RegExp(project));
+  });
+
+  it('creates .editorconfig', function () {
+    assert.file(['.editorconfig']);
+  });
+
+  it('creates react / flux skeleton', function () {
+    assert.file([
+      'app/assets/index.html',
+      'app/scripts/actions/.keep',
+      'app/scripts/components/App.jsx',
+      'app/scripts/constants/.keep',
+      'app/scripts/dispatcher/dispatcher.js',
+      'app/scripts/services/.keep',
+      'app/scripts/stores/.keep',
+      'app/scripts/initialize.jsx',
+      'app/scripts/routes.jsx'
+    ]);
+  });
+
+  it('creates sass project structure', function () {
+    assert.file([
+      'app/styles/modules/.keep',
+      'app/styles/partials/.keep',
+      'app/styles/app.scss'
+    ]);
+  });
+});
